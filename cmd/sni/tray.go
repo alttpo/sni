5f59a4fd81db06d7182bbca5b8a84a158b7f75c6
jsd1982 <james.jdunne@gmail.com>
2021-06-18 17:07:48 -0500

    sni: fix windows crash on startup by supplying example icon data

--- cmd/sni/tray.go
+++ cmd/sni/tray.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"github.com/getlantern/systray"
+	"github.com/getlantern/systray/example/icon"
 )
 
 func createSystray() {
@@ -22,7 +23,7 @@ func trayExit() {
 
 func trayStart() {
 	// Set up the systray:
-	//systray.SetTemplateIcon(icon.Data, icon.Data)
+	systray.SetTemplateIcon(icon.Data, icon.Data)
 	//systray.SetTitle("SNI")
 	systray.SetTooltip("SNI")
 	//mOpenWeb := systray.AddMenuItem("Web UI", "Opens the web UI in the default browser")
