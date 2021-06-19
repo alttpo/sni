13a6077024326d90213d21131cd957db23fdf2d2
jsd1982 <james.jdunne@gmail.com>
2021-06-19 00:35:45 -0500

    examples: removed redundant copy of sni.proto and modified generate.sh to point to original copy

--- examples/nodejs/generate.sh
+++ examples/nodejs/generate.sh
@@ -1,1 +1,1 @@
-grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./ --grpc_out=grpc_js:. sni.proto
+grpc_tools_node_protoc --proto_path=../../protos/sni --js_out=import_style=commonjs,binary:./ --grpc_out=grpc_js:. ../../protos/sni/sni.proto
