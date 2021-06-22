857631683912d059b557033d6fd65d93a5619d26
jsd1982 <james.jdunne@gmail.com>
2021-06-21 22:12:16 -0500

    snes: define CodedError to pair a codes.Code with an error

--- snes/errors.go
+++ snes/errors.go
@@ -1,6 +1,9 @@
 package snes
 
-import "fmt"
+import (
+	"fmt"
+	"google.golang.org/grpc/codes"
+)
 
 type ErrDeviceDisconnected struct {
 	wrapped error
@@ -10,3 +13,14 @@ func (e ErrDeviceDisconnected) Unwrap() error { return e.wrapped }
 func (e ErrDeviceDisconnected) Error() string {
 	return fmt.Sprintf("snes device disconnected: %v", e.wrapped)
 }
+
+
+type CodedError struct {
+	codes.Code
+	Cause error
+}
+
+func (e *CodedError) Error() string { return e.Cause.Error() }
+func (e *CodedError) Unwrap() error { return e.Cause }
+
+func WithCode(code codes.Code, cause error) *CodedError { return &CodedError{code, cause} }
