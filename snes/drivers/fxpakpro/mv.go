56b29ada10ce503bb031fd170bd378c57e850cdc
jsd1982 <james.jdunne@gmail.com>
2021-07-01 12:01:18 -0500

    fxpakpro: implement mv() which apparently can only rename files in the same folder, not move them from one folder to another as one would expect

---
+++ snes/drivers/fxpakpro/mv.go
@@ -0,0 +1,49 @@
+package fxpakpro
+
+import (
+	"context"
+	"fmt"
+)
+
+// mv does not allow moving files between folders; only renaming a file in an existing folder to a new filename
+func (d *Device) mv(ctx context.Context, path, newFilename string) (err error) {
+	sb := make([]byte, 512)
+	sb[0], sb[1], sb[2], sb[3] = byte('U'), byte('S'), byte('B'), byte('A')
+	sb[4] = byte(OpMV)
+	sb[5] = byte(SpaceFILE)
+	sb[6] = byte(FlagNONE)
+
+	// copy in the current path to position 256:
+	nameBytes := []byte(path)
+	copy(sb[256:512], nameBytes)
+	// copy in the new filename to position 8:
+	copy(sb[8:256], newFilename)
+
+	if shouldLock(ctx) {
+		defer d.lock.Unlock()
+		d.lock.Lock()
+	}
+
+	// send command:
+	err = sendSerial(d.f, 512, sb)
+	if err != nil {
+		_ = d.Close()
+		return
+	}
+
+	// read response:
+	err = recvSerial(d.f, sb, 512)
+	if err != nil {
+		_ = d.Close()
+		return
+	}
+	if sb[0] != 'U' || sb[1] != 'S' || sb[2] != 'B' || sb[3] != 'A' {
+		_ = d.Close()
+		return fmt.Errorf("mv: fxpakpro response packet does not contain USBA header")
+	}
+	if ec := sb[5]; ec != 0 {
+		return fmt.Errorf("mv: %w", fxpakproError(ec))
+	}
+
+	return
+}
---
+++ snes/drivers/fxpakpro/mv_test.go
@@ -0,0 +1,53 @@
+package fxpakpro
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+)
+
+func TestDevice_mv(t *testing.T) {
+	d := openExactDevice(t)
+	defer d.Close()
+
+	type args struct {
+		ctx         context.Context
+		path        string
+		newFilename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "mv /unittest/test1 test2",
+			args: args{
+				ctx:         context.Background(),
+				path:        "unittest/test1",
+				newFilename: "test2",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := d.mkdir(tt.args.ctx, filepath.Dir(tt.args.path)); err != nil {
+				t.Logf("mkdir() error (ignored) = %v\n", err)
+			}
+
+			if err := d.putFile(tt.args.ctx, putFileRequest{
+				path:   tt.args.path,
+				rom:    []byte{1},
+				report: nil,
+			}); err != nil {
+				t.Errorf("putFile() error = %v", err)
+				return
+			}
+
+			if err := d.mv(tt.args.ctx, tt.args.path, tt.args.newFilename); (err != nil) != tt.wantErr {
+				t.Errorf("mv() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
