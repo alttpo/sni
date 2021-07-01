618a294c672cd3232d70944a2a6c9b64d7325d70
jsd1982 <james.jdunne@gmail.com>
2021-07-01 10:08:18 -0500

    fxpakpro: implement rm()

---
+++ snes/drivers/fxpakpro/rm.go
@@ -0,0 +1,46 @@
+package fxpakpro
+
+import (
+	"context"
+	"fmt"
+)
+
+func (d *Device) rm(ctx context.Context, path string) (err error) {
+	sb := make([]byte, 512)
+	sb[0], sb[1], sb[2], sb[3] = byte('U'), byte('S'), byte('B'), byte('A')
+	sb[4] = byte(OpRM)
+	sb[5] = byte(SpaceFILE)
+	sb[6] = byte(FlagNONE)
+
+	// copy in the name to position 256:
+	nameBytes := []byte(path)
+	copy(sb[256:512], nameBytes)
+
+	if shouldLock(ctx) {
+		defer d.lock.Unlock()
+		d.lock.Lock()
+	}
+
+	// send command:
+	err = sendSerial(d.f, sb)
+	if err != nil {
+		_ = d.Close()
+		return
+	}
+
+	// read response:
+	err = recvSerial(d.f, sb, 512)
+	if err != nil {
+		_ = d.Close()
+		return
+	}
+	if sb[0] != 'U' || sb[1] != 'S' || sb[2] != 'B' || sb[3] != 'A' {
+		_ = d.Close()
+		return fmt.Errorf("rm: fxpakpro response packet does not contain USBA header")
+	}
+	if ec := sb[5]; ec != 0 {
+		return fmt.Errorf("rm: %w", fxpakproError(ec))
+	}
+
+	return
+}
---
+++ snes/drivers/fxpakpro/rm_test.go
@@ -0,0 +1,44 @@
+package fxpakpro
+
+import (
+	"context"
+	"testing"
+)
+
+func TestDevice_rm(t *testing.T) {
+	d := openExactDevice(t)
+
+	type args struct {
+		ctx  context.Context
+		path string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "rm /unittest/sub",
+			args: args{
+				ctx:  context.Background(),
+				path: "unittest/sub",
+			},
+			wantErr: false,
+		},
+		{
+			name: "rm /unittest",
+			args: args{
+				ctx:  context.Background(),
+				path: "unittest",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := d.rm(tt.args.ctx, tt.args.path); (err != nil) != tt.wantErr {
+				t.Errorf("rm() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
