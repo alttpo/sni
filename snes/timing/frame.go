09a1fa8a8a47ef5cef9cb94ac8b05233327d3a13
jsd1982 <james.jdunne@gmail.com>
2021-07-17 20:59:04 -0500

    snes: better comment on timing

--- snes/timing/frame.go
+++ snes/timing/frame.go
@@ -2,5 +2,5 @@ package timing
 
 import "time"
 
-// Frame = 5,369,317.5 / 89,341.5 ~= 60.0988 frames / sec ~= 16,639,265.605 ns / frame
+// Frame = 5,369,317.5 / 89,341.5 ~= 60.0988062658451 frames / sec ~= 16,639,265.605 ns / frame
 const Frame = time.Nanosecond * 16_639_265
