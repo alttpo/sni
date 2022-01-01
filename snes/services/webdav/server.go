1e7feb17168739e05dce9dd4809cea7469c58b2e
jsd1982 <james.jdunne@gmail.com>
2021-12-31 21:00:51 -0600

    webdav: simple empty filesystem impl

--- snes/services/webdav/server.go
+++ snes/services/webdav/server.go
@@ -3,8 +3,9 @@ package webdav
 import (
 	"context"
 	dav "golang.org/x/net/webdav"
+	"io/fs"
+	"log"
 	"os"
-	"sni/snes"
 )
 
 // AdapterFileSystem adapts the underlying simplistic snes.DeviceFilesystem interface to the more flexible
@@ -14,38 +15,63 @@ import (
 // must ensure that reading or writing is done sequentially and must not allow seeking after a read or write
 // has started.
 type AdapterFileSystem struct {
-	fs snes.DeviceFilesystem
+	//fs snes.DeviceFilesystem
 }
 
 func (a *AdapterFileSystem) Mkdir(ctx context.Context, name string, perm os.FileMode) error {
+	log.Printf("mkdir(%#v, %#v)\n", name, perm)
+
 	_ = perm
-	return a.fs.MakeDirectory(ctx, name)
+	//return a.fs.MakeDirectory(ctx, name)
+	return nil
 }
 
 func (a *AdapterFileSystem) OpenFile(ctx context.Context, name string, flag int, perm os.FileMode) (f dav.File, err error) {
+	defer func() {
+		log.Printf("%p = openFile(%#v, %#v, %#v)\n", f, name, flag, perm)
+	}()
 	if flag&os.O_RDWR != 0 {
 		// cannot open files in both read/write mode:
 		err = os.ErrInvalid
 		return
 	}
 	if flag&os.O_WRONLY != 0 {
-		f = &writeable{fs: a.fs, name: name}
+		f = &writeable{name: name}
 		return
 	} else {
-		f = &readable{fs: a.fs, name: name}
+		// readable open:
+		var stat os.FileInfo
+		stat, err = a.Stat(ctx, name)
+		if err != nil {
+			return
+		}
+
+		f = NewReadable(a, name, stat)
 		return
 	}
 }
 
 func (a *AdapterFileSystem) RemoveAll(ctx context.Context, name string) error {
-	//TODO implement me
+	log.Printf("removeAll(%#v)\n", name)
 	panic("implement me")
 }
 
 func (a *AdapterFileSystem) Rename(ctx context.Context, oldName, newName string) error {
-	return a.fs.RenameFile(ctx, oldName, newName)
+	log.Printf("rename(%#v, %#v)\n", oldName, newName)
+	return nil
+	//return a.fs.RenameFile(ctx, oldName, newName)
 }
 
-func (a *AdapterFileSystem) Stat(ctx context.Context, name string) (os.FileInfo, error) {
-	return nil, nil
+func (a *AdapterFileSystem) Stat(ctx context.Context, name string) (stat os.FileInfo, err error) {
+	log.Printf("stat(%#v)\n", name)
+
+	if name == "" {
+		stat = &fileInfo{
+			name:  "",
+			isDir: true,
+		}
+		return
+	}
+
+	return nil, fs.ErrNotExist
 }
--- snes/services/webdav/writable.go
+++ snes/services/webdav/writable.go
@@ -2,40 +2,43 @@ package webdav
 
 import (
 	"io/fs"
+	"log"
 	"os"
-	"sni/snes"
 )
 
 type writeable struct {
-	fs   snes.DeviceFilesystem
+	//fs   snes.DeviceFilesystem
 	name string
+	stat fileInfo
 }
 
 func (f *writeable) Close() error {
-	//TODO implement me
-	panic("implement me")
+	log.Printf("%p.close()\n", f)
+	return nil
 }
 
 func (f *writeable) Read(p []byte) (n int, err error) {
+	log.Printf("%p.read(%p)\n", f, p)
 	err = os.ErrInvalid
 	return
 }
 
 func (f *writeable) Seek(offset int64, whence int) (n int64, err error) {
-	err = os.ErrInvalid
-	return
+	log.Printf("%p.seek(%#v, %#v)\n", f, offset, whence)
+	return 0, ErrSeekForbidden
 }
 
 func (f *writeable) Readdir(count int) (fis []fs.FileInfo, err error) {
-	//TODO implement me
-	panic("implement me")
+	log.Printf("%p.readdir(%#v)\n", f, count)
+	return nil, fs.ErrInvalid
 }
 
 func (f *writeable) Stat() (fi fs.FileInfo, err error) {
-	//TODO implement me
-	panic("implement me")
+	log.Printf("%p.stat()\n", f)
+	return &f.stat, nil
 }
 
 func (f *writeable) Write(p []byte) (n int, err error) {
-	panic("TODO")
+	log.Printf("%p.write(%p)\n", f, p)
+	return 0, nil
 }
