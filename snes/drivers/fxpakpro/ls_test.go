b65c87fae08a8a04429de0ea72cef6602a32d8b7
jsd1982 <james.jdunne@gmail.com>
2022-01-07 08:54:04 -0600

    fxpakpro: LS opcode tests and runaway directory list error

--- snes/drivers/fxpakpro/ls_test.go
+++ snes/drivers/fxpakpro/ls_test.go
@@ -26,6 +26,41 @@ func TestDevice_listFiles(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "list ''",
+			args: args{
+				path: "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "list 'o2'",
+			args: args{
+				path: "o2",
+			},
+			wantErr: false,
+		},
+		{
+			name: "list 'romloader'",
+			args: args{
+				path: "romloader",
+			},
+			wantErr: false,
+		},
+		{
+			name: "list 'o2'",
+			args: args{
+				path: "o2",
+			},
+			wantErr: false,
+		},
+		{
+			name: "list ''",
+			args: args{
+				path: "",
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
