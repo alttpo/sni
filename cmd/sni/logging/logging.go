20c5494b44b89b30f336c90bf24a31e57cfdff52
James Dunne <james.jdunne@gmail.com>
2022-01-31 20:30:08 -0600

    sni: include GOOS, GOARCH in app version; include app version in log filenames

--- cmd/sni/logging/logging.go
+++ cmd/sni/logging/logging.go
@@ -40,7 +40,7 @@ func Init() {
 	ts := time.Now().Format("2006-01-02T15:04:05.000Z")
 	ts = strings.ReplaceAll(ts, ":", "-")
 	ts = strings.ReplaceAll(ts, ".", "-")
-	Path = filepath.Join(Dir, fmt.Sprintf("sni-%s.log", ts))
+	Path = filepath.Join(Dir, fmt.Sprintf("sni-%s-%s-%s-%s.log", runtime.GOOS, runtime.GOARCH, appversion.Version, ts))
 
 	// open the log file for writing:
 	logFile, err := os.OpenFile(Path, os.O_CREATE|os.O_WRONLY, 0644)
@@ -52,11 +52,12 @@ func Init() {
 
 	// first line should be the app version:
 	log.Printf(
-		"sni %s %s built on %s by %s",
+		"sni-%s-%s %s %s built on %s by %s",
+		runtime.GOOS,
+		runtime.GOARCH,
 		appversion.Version,
 		appversion.Commit,
 		appversion.Date,
-		appversion.BuiltBy,
-	)
+		appversion.BuiltBy)
 	log.Printf("logging to '%s'\n", Path)
 }
