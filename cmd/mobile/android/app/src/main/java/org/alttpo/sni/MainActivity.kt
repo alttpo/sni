d78c002db15b0f2886243eb03e6a5eeb06a468bc
jsd1982 <james.jdunne@gmail.com>
2022-05-30 22:25:48 -0500

    android: update to use foreground service to avoid being killed

--- cmd/mobile/android/app/src/main/java/org/alttpo/sni/MainActivity.kt
+++ cmd/mobile/android/app/src/main/java/org/alttpo/sni/MainActivity.kt
@@ -1,9 +1,11 @@
 package org.alttpo.sni
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ActivityManager
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,17 +15,30 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btnStart)
         val btnStop = findViewById<Button>(R.id.btnStop)
 
-        btnStop.isEnabled = false
+        // set initial button state based on service running status:
+        if (isServiceRunning<SNIService>()) {
+            btnStart.isEnabled = false
+            btnStop.isEnabled = true
+        } else {
+            btnStart.isEnabled = true
+            btnStop.isEnabled = false
+        }
 
         btnStart.setOnClickListener {
-            startService(Intent(this, SNIService::class.java))
+            SNIService.startService(this)
             btnStop.isEnabled = true
             btnStart.isEnabled = false
         }
         btnStop.setOnClickListener {
             btnStop.isEnabled = false
             btnStart.isEnabled = true
-            stopService(Intent(this, SNIService::class.java))
+            SNIService.stopService(this)
         }
     }
+
+    @Suppress("DEPRECATION") // Deprecated for third party Services.
+    inline fun <reified T> Context.isServiceRunning() =
+        (getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
+            .getRunningServices(Integer.MAX_VALUE)
+            .any { it.service.className == T::class.java.name }
 }
--- cmd/mobile/android/app/src/main/java/org/alttpo/sni/SNIService.kt
+++ cmd/mobile/android/app/src/main/java/org/alttpo/sni/SNIService.kt
@@ -1,17 +1,85 @@
 package org.alttpo.sni
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import java.text.MessageFormat
 
 private const val TAG = "SNIService"
 
 class SNIService : Service() {
+    private val CHANNEL_ID = "SNIService Channel"
+
+    companion object {
+        fun startService(context: Context) {
+            val startIntent = Intent(context, SNIService::class.java)
+            //startIntent.putExtra("inputExtra", message)
+            Log.i(TAG, "startForegroundService")
+            ContextCompat.startForegroundService(context, startIntent)
+        }
+        fun stopService(context: Context) {
+            val stopIntent = Intent(context, SNIService::class.java)
+            Log.i(TAG, "stopService")
+            context.stopService(stopIntent)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.i(TAG, "onStartCommand")
+
+        createNotificationChannel()
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        Log.i(TAG, "PendingIntent.getActivity")
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            notificationIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        Log.i(TAG, "NotificationCompat.Builder")
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("SNI")
+            .setContentText("Running")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        Log.i(TAG, "startForeground(1, notification)")
+        startForeground(1, notification)
+
+        // start up SNI native service:
+        Log.i(TAG, "mobile.Mobile.start()")
         mobile.Mobile.start()
-        return super.onStartCommand(intent, flags, startId)
+
+        return START_NOT_STICKY
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Log.i(TAG, "NotificationChannel()")
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID,
+                "SNI Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            Log.i(TAG, "getSystemService(NotificationManager)")
+            val manager = getSystemService(NotificationManager::class.java)
+            Log.i(TAG, "manager.createNotificationChannel()")
+            manager!!.createNotificationChannel(serviceChannel)
+        } else {
+            Log.i(TAG, MessageFormat.format("Build.VERSION.SDK_INT = {0}", Build.VERSION.SDK_INT))
+        }
     }
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -22,6 +90,7 @@ class SNIService : Service() {
     override fun onDestroy() {
         Log.i(TAG, "onDestroy")
         mobile.Mobile.stop()
+
         super.onDestroy()
     }
 }
