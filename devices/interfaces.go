3a31be6bad63afbf99c63eaa403bbffe3c3500f6
jsd1982 <james.jdunne@gmail.com>
2022-12-20 15:07:12 -0600

    domains: WIP on domains.yaml declarative mappings per driver/core

--- devices/interfaces.go
+++ devices/interfaces.go
@@ -28,7 +28,8 @@ type DeviceDescriptor struct {
 	Kind                string
 	Capabilities        []sni.DeviceCapability
 	DefaultAddressSpace sni.AddressSpace
-	System              string
+	// deprecated
+	System string
 }
 
 // Device acts as an exclusive-access gateway to the subsystems of the SNES device
